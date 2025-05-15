278393a19fa90aa2776507095c47c33fcfb5c6a8
Danya76131 <danylo.galetskiy@gmail.com>
2025-05-15 18:04:20 +0200

    js,css,html in dev

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/artists.js';
+import './js/artists-modal.js';
+import './js/about-us.js';
+import './js/feedback.js';
+import './js/footer.js';
